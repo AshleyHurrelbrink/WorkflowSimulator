84749213f42d3bda56b3b8b517e465f49670f459
AshleyHurrelbrink <ahurrelbrink98@yahoo.com>
2020-10-06 16:04:07 +0300

    Added base project

---
+++ src/home/Main.java
@@ -0,0 +1,23 @@
+package home;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
+        primaryStage.setTitle("WorkflowSimulator");
+        primaryStage.setScene(new Scene(root, 663, 421));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
