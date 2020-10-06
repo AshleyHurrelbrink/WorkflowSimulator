f0862c27f7f7cdc9d7c1858c0f9e2dbe976db775
AshleyHurrelbrink <ahurrelbrink98@yahoo.com>
2020-10-06 20:57:10 +0300

    Changed size of template and added an empty css file

--- src/home/Main.java
+++ src/home/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
         primaryStage.setTitle("WorkflowSimulator");
-        primaryStage.setScene(new Scene(root, 687, 442));
+        primaryStage.setScene(new Scene(root, 700, 442));
         primaryStage.show();
     }
 
